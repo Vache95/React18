07e152a2296e7f37d1e9650f72623d5cdbaab531
Vache <vache.virabyan@codetime.am>
2022-05-28 13:58:40 +0400

    input mask

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {React,useState, useEffect, useRef} from 'react'
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+
+    const [card, setCard] = useState();
+    const inputCard = useRef();
+  
+    const handleChange = () => {
+      const cardValue = inputCard.current.value.replace(/\D/g, '').match(/(\d{0,4})(\d{0,4})(\d{0,4})(\d{0,4})/);
+      inputCard.current.value = !cardValue[2]
+        ? cardValue[1]
+        : `${cardValue[1]} ${cardValue[2]}${`${
+            cardValue[3] ? ` ${cardValue[3]}` : ''
+          }`}${`${cardValue[4] ? ` ${cardValue[4]}` : ''}`}`;
+      const numbers = inputCard.current.value.replace(/(\D)/g, '');
+      setCard(numbers);
+    };
+  
+    // useEffect(() => {
+    //   handleChange();
+    // }, [card]);
+
+ return (
+   <div className="wrapper">
+      <form className='form'>
+          <div className="form__input1">
+          <input type="text" ref={inputCard} onChange={handleChange} />
+            {/* <input type="number" /> */}
+            {/* <input id="ccn" type="tel" inputmode="numeric" pattern="[0-9\s]{13,19}" autocomplete="cc-number" maxlength="19" placeholder="xxxx xxxx xxxx xxxx"></input> */}
+          </div>
+          <div className="form__input2">
+            <input type="number" />
+          </div>
+          <button type='submit'>SEND</button>
+      </form>
+   </div>
+  )
 }
 
-export default App;
